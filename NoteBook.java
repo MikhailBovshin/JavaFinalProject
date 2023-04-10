c43d9a0bbcfb1ace3019dedf3a13f8f3a7776e23
Mikhail Bovshin <mih.bovshin@gmail.com>
2023-04-10 15:45:21 +0300

    initial commit

---
+++ NoteBook.java
@@ -0,0 +1,30 @@
+public class NoteBook {
+    private int memory;
+    private int hd;
+    private String os;
+    private String color;
+
+    public NoteBook (int memory, int hd, String os, String color) {
+        this.memory = memory;
+        this.hd = hd;
+        this.os = os;
+        this.color = color;
+    }
+
+    // public void setMemory(int memory) {
+    //     this.memory = memory;
+    // }
+
+    // public void setHd(int hd) {
+    //     this.hd = hd;
+    // }
+
+    // public void setOs(String os) {
+    //     this.os = os;
+    // }
+
+    // public void setColor(String color) {
+    //     this.color = color;
+    // }
+
+}
---
+++ finalTest.java
@@ -0,0 +1,9 @@
+/**
+ * finalTest
+ */
+public class finalTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
